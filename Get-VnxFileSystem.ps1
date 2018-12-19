fb73dee8ade9e9325f9702652a3f377ce54441ee
nightcrawler086 <brian.hall4@gmail.com>
2018-12-19 12:28:26 -0800

    Added filters/aspects

--- Get-VnxFileSystem.ps1
+++ Get-VnxFileSystem.ps1
@@ -1,108 +1,132 @@
 ﻿<#
 .SYNOPSIS
-   This cmdlet is used to get a list of all filesystems on a VNX
+   This cmdlet queries filesystems on the VNX
 .DESCRIPTION
-   This cmdlet returns all existing filesystems on the VNX.  If querying
-   for a specific filesytem, use Get-VnxFilesystem
+   This cmdlet returns 
 .EXAMPLE
    PS > Get-VnxFilesystem
 #>
-function Get-VnxFileSystem
-{
+#function Get-VnxFileSystem {
     [CmdletBinding()]
     Param
     (
-        [Parameter(Mandatory=$false,
+        # Get the filesystem by name
+        [Parameter(Mandatory=$true,
          ValueFromPipelineByPropertyName=$true,
+         ParameterSetName = "ByName",
          Position=1)]
-         [ValidateNotNullOrEmpty()]
-         $Name,
-        
-         [Parameter(Mandatory=$false,
+        [ValidateNotNullOrEmpty()]
+         [string]$Name,
+        # Get the filesystem by ID
+        # Apparently this doesn't work either
+        #[Parameter(Mandatory=$true,
+        # ValueFromPipelineByPropertyName=$true,
+        # ParameterSetName = "ById",
+        # Position=2)]
+        #[ValidateNotNullOrEmpty()]
+        # [int]$Id,
+        # Get filesystems by physical data mover
+        [Parameter(Mandatory=$true,
          ValueFromPipelineByPropertyName=$true,
-         Position=2)]
-         [ValidateNotNullOrEmpty()]
-         $Mover,
-
-         [Parameter(Mandatory=$false,
-         ValueFromPipelineByPropertyName=$true,
-         Position=1)]
-         [ValidateNotNullOrEmpty()]
-         $Vdm,
-        # Switch Parameters for aspect selections
-        [Parameter(Mandatory=$false,
-         ValueFromPipelineByPropertyName=$false,
-         Position=2)]
+         ParameterSetName = "ByMover",
+         Position=3)]
+        [ValidateNotNullOrEmpty()]
+         [int]$Mover,
+        # Get filesystems by virtual data mover
+        [Parameter(Mandatory=$true,
+          ValueFromPipelineByPropertyName=$true,
+          ParameterSetName = "ByVdm",
+          Position=4)]
+        [ValidateNotNullOrEmpty()]
+         [int]$Vdm,
+        # Get fsCapabilities properties
+        [Parameter(Mandatory=$false)]
          [switch]$FsCapabilities,
-
-         [Parameter(Mandatory=$false,
-         ValueFromPipelineByPropertyName=$false,
-         Position=2)]
-         [switch]$FsCheckpointInfo,
-         
-         [Parameter(Mandatory=$false,
-         ValueFromPipelineByPropertyName=$false,
-         Position=2)]
-         [switch]$FsDhsmInfo,
-
-         [Parameter(Mandatory=$false,
-         ValueFromPipelineByPropertyName=$false,
-         Position=2)]
-         [switch]$FsRdeInfo
+        # Get filesystem checkpoint information
+        [Parameter(Mandatory=$false)]
+         [switch]$FsCheckpointInfo
     )
     BEGIN {
         # Expecting a Global variable to be set called
         # CurrentVnxSystem which contains the existing
         # session.
         If (!$CurrentVnxFrame) {
-            Write-Host -ForegroundColor Red "Not currently connected to a VNX System."
-            Write-Host -ForegroundColor Red "Run Connect-VnxSystem first."
+            Write-Host -ForegroundColor Yellow "Not currently connected to a VNX System."
+            Write-Host -ForegroundColor Yellow "Run Connect-VnxSystem first."
             Exit 1
         }
-        # This is the query URL
+        # This is the query URL, created using the hostname in CurrentVnxFrame global variable
         $apiuri = "https://$($CurrentVnxFrame.HostName)/servlets/CelerraManagementServices"
-        #write-host $apiuri
-        # Setting header
+        # Setting header for the HTML request
         $header = @{"Content-Type" = "x-www-form-urlencoded"}
-        # Standard "top" of XML Sheet
-        $xmltop = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>'
+        # XML sheet declaration
+        $xmldec = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>'
         # Standard format of XML Sheet
-        $xmlformat = '<RequestPacket xmlns="http://www.emc.com/schemas/celerra/xml_api" >'
+        $reqopen = '<RequestPacket xmlns="http://www.emc.com/schemas/celerra/xml_api" >'
         # Standard Footer for XML Sheet
-        $xmlfooter = '</RequestPacket>'
-        # Query for CIFS Shares for entire frame
+        $reqclose = '</RequestPacket>'
+        # Open the query node
         $qryopen = '<Request><Query>'
-        $qrybegin = "<FileSystemQueryParams>"
-        $aspects = "<AspectSelection fileSystems=""true"" fileSystemCapacityInfos=""true"" />"  
-        
+        # Open the filesystem query node
+        $qrybegin = "<FileSystemQueryParams>"  
+        # Close the filesystem query node
         $qryend =  "</FileSystemQueryParams>"
+        # Close the query
         $qryclose = '</Query></Request>'
-        # Adding all the pieces together
-        If (!$Name) {
-            <#
-            [xml]$request = New-Object System.Xml.XmlDocument
-            $dec = $request.CreateXmlDeclaration("1.0", "UTF-8", "yes")
-            $request.AppendChild($dec)
-            $request.AppendChild($xmlformat)
-            $request.AppendChild($qryopen)
-            $request.AppendChild($qrybegin)
-            $request.AppendChild($qryend)
-            $request.AppendChild($qryclose)
-            $request.AppendChild($xmlfooter)
-            write-host $request
-            #>
-            $request = $xmltop + $xmlformat + $qryopen + $qrybegin + $aspects + $qryend + $qryclose + $xmlfooter   
+        # This is where we decide if we're going to filter the filesystems by
+        # name or mover.
+        If ($Name) {
+            # Going to query a filesystem by name
+            $filter = '<Alias name="'+ ${Name} + '" />'
+        }
+        ElseIf ($Id) {
+            # Going to query a filesystem by Id
+            $filter = '<FileSystem fileSystem="' + ${Id} + '" />'
+        }
+        ElseIf ($Mover) {
+            # Going to query filesystems by physical datamover
+            $filter = '<Mover mover="' + ${Mover} + '" />'
+        }
+        ElseIf ($Vdm) {
+            # Going to query filesystems by VDM
+            $filter = '<Vdm vdm="' + ${Vdm} + '" />'
         }
         Else {
-            $filter = '<Alias name=""${Name}"" />'
-            $request = $xmltop + $xmlformat + $qryopen + $qrybegin + $aspects + $filter + $qryend + $qryclose + $xmlfooter
+            # No filter specified, retrieve all filesystems
+            $filter = ""
         }
+        # Defining all aspects available, to build the selection list
+        $asp1 = 'fileSystems="true" '
+        $asp2 = 'fileSystemCapacityInfos="true" '
+        $asp3 = 'fileSystemCapabilities="true" '
+        $asp4 = 'fileSystemCheckpointInfos="true" '
+        # Need to figure out how to build the aspect selections
+        $aspects = $asp1 + $asp2 
+        # This next set of statements switches the options on/off according to the switches passed
+        If ($FsCapabilities) {
+            $aspects = $aspects + $asp3
+        }
+        If ($FsCheckpointInfo) {
+            $aspects = $aspects + $asp4
+        }
+        $aspects = '<AspectSelection ' + $aspects + '/>'
+        #write-host $aspects
+        # Put all the pieces together to build the XML document
+        If ($filter) {
+            $body = $xmldec + $reqopen + $qryopen + $qrybegin + $aspects + $filter + $qryend + $qryclose + $reqclose
+        }
+        Else {
+            $body = $xmldec + $reqopen + $qryopen + $qrybegin + $aspects + $qryend + $qryclose + $reqclose
+            #Write-host $body
+        }
+    
     }
     PROCESS {
-        $response = Invoke-RestMethod -Uri $apiuri -WebSession $CurrentVnxFrame.Session -Headers $header -Body $request -Method Post
+        $response = Invoke-RestMethod -Uri $apiuri -WebSession $CurrentVnxFrame.Session -Headers $header -Body $body -Method Post
+        #Need to 
     }
     END {
-        $response.responsepacket.response.Filesystem
+        $response
     }
-}
-Get-VnxFileSystem
+#}
+#Get-VnxFileSystem
